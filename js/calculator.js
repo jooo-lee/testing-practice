ae54f01e8fb9c73574c51f861a3ecc2b87a11f89
Joseph Lee <josephlee3004@gmail.com>
2024-04-29 19:43:46 -0400

    Create calculator object

---
+++ js/calculator.js
@@ -0,0 +1,9 @@
+const createCalculator = () => {
+    const add = (x, y) => x + y;
+    const subtract = (x, y) => x - y;
+    const divide = (x, y) => x / y;
+    const multiply = (x, y) => x * y;
+    return { add, subtract, divide, multiply };
+};
+
+export default createCalculator;
